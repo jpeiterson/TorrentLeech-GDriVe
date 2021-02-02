64dc64b48abc8ee8476aecb9a642e3926e652124
jpeiterson <78341388+jpeiterson@users.noreply.github.com>
2021-02-02 02:36:00 -0800

    Delete config.py

--- tobrot/config.py
+++
@@ -1,7 +0,0 @@
-from tobrot.sample_config import Config
-
-class Config(Config):
-  TG_BOT_TOKEN = "1672470007:AAFuiJrJZBbeSK91rHpD7GwiwIFf0ca4Vtk"
-  APP_ID = "2658282"
-  API_HASH = "f7cd1875130be49230196f02c6cc540c"
-  AUTH_CHANNEL = "-1001347208227"
